7796e2bac544e07b1c67cb4e6e2ef0198bd45196
Kunal Goyal <kunalgoyal.goyal9@gmail.com>
2020-06-01 03:59:46 +0530

    sync

--- distiller/apputils/image_classifier.py
+++ distiller/apputils/image_classifier.py
@@ -603,7 +603,9 @@ def train(train_loader, model, criterion, optimizer, epoch,
         # Measure data loading time
         data_time.add(time.time() - end)
         # print("compression_scheduler: ", args.device)
-        inputs, target = inputs.to(args.device)/10000, target.to(args.device)
+        inputs, target = inputs.to(args.device), target.to(args.device)
+        
+        print("target: ", target)
         
         # print("inputs: ", inputs)
 
