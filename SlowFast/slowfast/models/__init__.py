c6470afe3681e0650a9d5c6816bb603fd356d8b3
Kunal Goyal <kunalgoyal.goyal9@gmail.com>
2020-06-03 19:54:10 +0530

    sync

--- SlowFast/slowfast/models/__init__.py
+++ SlowFast/slowfast/models/__init__.py
@@ -4,3 +4,5 @@
 from .build import MODEL_REGISTRY, build_model  # noqa
 from .custom_video_model_builder import *  # noqa
 from .video_model_builder import ResNet, SlowFast  # noqa
+
+print("*******" * 100)
