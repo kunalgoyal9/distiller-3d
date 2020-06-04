4435b748c9a403faffd7d89e23fd5f6df048b9e2
Kunal Goyal <kunalgoyal.goyal9@gmail.com>
2020-06-04 13:14:33 +0530

    sync

--- distiller/apputils/image_classifier.py
+++ distiller/apputils/image_classifier.py
@@ -602,6 +602,8 @@ def train(train_loader, model, criterion, optimizer, epoch,
     
     first_chk = True
 
+    print("&&"*100)
+
     for train_step, (inputs, target) in enumerate(train_loader):
         # Measure data loading time
         data_time.add(time.time() - end)
