a302af04d5583b02c12361ec096a0e0526f8c4f8
Kunal Goyal <kunalgoyal.goyal9@gmail.com>
2020-07-19 19:22:13 +0530

    sync

--- distiller/apputils/image_classifier.py
+++ distiller/apputils/image_classifier.py
@@ -611,13 +611,17 @@ def train(train_loader, model, criterion, optimizer, epoch,
         # Measure data loading time
         # print("Hello my friend")
 
+        print("inputs: ", inputs)
+        print("target: ", target)
+
+
         data_time.add(time.time() - end)
         # print("compression_scheduler: ", args.device)
         
         # print("inputs: ", inputs,"target: ", target)
 
         if args.dataset == 'ucf101':
-            inputs, target = inputs.to(args.device)/255, target.to(args.device)
+            inputs, target = inputs.to(args.device), target.to(args.device)
         else:
             inputs[0], inputs[1], target = inputs[0].to(args.device), inputs[1].to(args.device), target.to(args.device)
                     
