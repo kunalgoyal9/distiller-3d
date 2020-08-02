f42e77df0fcf145a87aced8198d5d4f7fe3b8412
Kunal Goyal <kunalgoyal.goyal9@gmail.com>
2020-08-02 22:34:03 +0530

    sync

--- distiller/apputils/image_classifier.py
+++ distiller/apputils/image_classifier.py
@@ -615,7 +615,7 @@ def train(train_loader, model, criterion, optimizer, epoch,
         # print(inputs)
 
         if args.dataset == 'ucf101':
-            inputs, target = inputs.to(args.device)/255, target.to(args.device)
+            inputs, target = inputs.to(args.device), target.to(args.device)
         else:
             inputs[0], inputs[1], target = inputs[0].to(args.device), inputs[1].to(args.device), target.to(args.device)
                     
@@ -625,7 +625,7 @@ def train(train_loader, model, criterion, optimizer, epoch,
 
         if not hasattr(args, 'kd_policy') or args.kd_policy is None:
             output = model(inputs)
-            # print("output: "  , output)
+            print("output: "  , output)
 
         else:
             output = args.kd_policy.forward(inputs)
