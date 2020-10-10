7826629ed36f77e1df95c445071cfe6dda81a212
kunal goyal <kunalgoyal.goyal9@gmail.com>
2020-10-10 14:50:14 +0530

    sync debugging

--- distiller/apputils/image_classifier.py
+++ distiller/apputils/image_classifier.py
@@ -614,7 +614,7 @@ def train(train_loader, model, criterion, optimizer, epoch,
         if args.dataset == 'slowfast_ucf101':
             inputs[0], inputs[1], target = inputs[0].to(args.device), inputs[1].to(args.device), target.to(args.device)
         elif args.dataset == 'i3d_ucf101':
-            print(inputs.shape)
+            print(inputs[0].shape)
             inputs[0], target = inputs[0].to(args.device), target.to(args.device)
         else:
             inputs, target = inputs.to(args.device), target.to(args.device)            
